b9d5ceefb94c342a8e431e947efdaafd92eefd91
Tres <tres.eberle@me.com>
2025-03-04 16:26:22 -0600

    switching to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
-import { AuthProvider } from './components/auth/AuthContext'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./components/auth/AuthContext";
 
-//import * as Page from  "./components" maybe
-import { PrivateRoute } from "./components/privateRoute/PrivateRoute"
-import { AuthPage } from "./components/auth/AuthPage"
-import { DashboardPage } from './components/dashboard/DashboardPage'
-import { NotFoundPage } from './components/notFound/NotFoundPage'
-import { TroopPage } from './components/troop/TroopPage'
-import { CreateTroopPage } from './components/troop/CreateTroopPage'
-import { JoinTroopPage } from './components/troop/JoinTroopPage'
-import { AwaitJoin } from './components/awaitJoin/AwaitJoin'
+import { PrivateRoute } from "./components/privateRoute/PrivateRoute";
+import { AuthPage } from "./components/auth/AuthPage";
+import { DashboardPage } from "./components/dashboard/DashboardPage";
+import { NotFoundPage } from "./components/notFound/NotFoundPage";
+import { TroopPage } from "./components/troop/TroopPage";
+import { CreateTroopPage } from "./components/troop/CreateTroopPage";
+import { JoinTroopPage } from "./components/troop/JoinTroopPage";
+import { AwaitJoin } from "./components/awaitJoin/AwaitJoin";
 
-const router = createBrowserRouter([
-  {
-    path: '/GirlScoutCookieTracker/',
-    element: <AuthPage />,
-    errorElement: <NotFoundPage />
-  },
-  {
-    path: '/GirlScoutCookieTracker/troop',
-    element: <PrivateRoute />,
-    children: [{ path: "/GirlScoutCookieTracker/troop", element: <TroopPage /> }],
-  },
-  {
-    path: '/GirlScoutCookieTracker/createTroop',
-    element: <PrivateRoute />,
-    children: [{ path: "/GirlScoutCookieTracker/createTroop", element: <CreateTroopPage /> }],
-  },
-  {
-    path: '/GirlScoutCookieTracker/joinTroop',
-    element: <PrivateRoute />,
-    children: [{ path: "/GirlScoutCookieTracker/joinTroop", element: <JoinTroopPage /> }],
-  },
-  {
-    path: '/GirlScoutCookieTracker/awaitJoin',
-    element: <AwaitJoin/>,
-  },
-  {
-    path: '/GirlScoutCookieTracker/dashboard',
-    element: <PrivateRoute />,
-    children: [{ path: "/GirlScoutCookieTracker/dashboard", element: <DashboardPage /> }],
-  },
-]);
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <StrictMode>
-      <RouterProvider router={router} basename="/GirlScoutCookieTracker" />
+      <HashRouter>
+        <Routes>
+          <Route path="/GirlScoutCookieTracker" element={<AuthPage />} />
+          <Route path="/GirlScoutCookieTracker/troop" element={<PrivateRoute><TroopPage /></PrivateRoute>} />
+          <Route path="/GirlScoutCookieTracker/createTroop" element={<PrivateRoute><CreateTroopPage /></PrivateRoute>} />
+          <Route path="/GirlScoutCookieTracker/joinTroop" element={<PrivateRoute><JoinTroopPage /></PrivateRoute>} />
+          <Route path="/GirlScoutCookieTracker/awaitJoin" element={<AwaitJoin />} />
+          <Route path="/GirlScoutCookieTracker/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </HashRouter>
     </StrictMode>
   </AuthProvider>
-  
-)
+);
