049193cc5cf2f27b1ecdc25903e10af80fbfac1f
Tres <tres.eberle@me.com>
2025-03-04 16:47:24 -0600

    switching to hashrouter

--- src/components/privateRoute/PrivateRoute.jsx
+++ src/components/privateRoute/PrivateRoute.jsx
@@ -13,4 +13,4 @@ export const PrivateRoute = () => {
     return <Outlet />;
 };
 
-
+ 
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/GirlScoutCookieTracker',
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        404: 'index.html',
-      },
-    },
-  },
+  plugins: [react()]
+  
 });
