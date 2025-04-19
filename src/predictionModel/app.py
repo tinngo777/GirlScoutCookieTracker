7a61fd376e10771a374fbfb880bba9d8dbc3377a
CoreyDCarlson <CoreydCarlson@outlook.com>
2025-04-19 16:24:13 -0500

    Changed port for Render

--- src/predictionModel/app.py
+++ src/predictionModel/app.py
@@ -94,4 +94,5 @@ def predict():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port if available
+    app.run(host="0.0.0.0", port=port)
